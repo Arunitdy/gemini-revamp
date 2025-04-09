38e37c8002f697a774f9beff58e841e0d2280014
Arun <arunmundakkal003@gmail.com>
2025-04-09 19:28:37 +0530

    feat: implement Tracks component with styling and hover effects

--- src/All/Tracks/Tracks.jsx
+++ src/All/Tracks/Tracks.jsx
@@ -1,8 +1,25 @@
 import "./Tracks.css";
+import icon1 from "../../img/fist.png";
+import icon2 from "../../img/innovation.png";
+import icon3 from "../../img/mental-health.png";
+import icon4 from "../../img/save-the-world.png";
 
 export const Tracks = () => {   
-    return(<>
-
-        
-    </>)
+    return(
+    <div className="Tracks"> 
+            <div className="track">
+                <a href=""><img src ={icon1} className="icon"/>
+                  <p>HealthTech</p>
+                </a>
+                <a href=""><img src ={icon2} className="icon"/>
+                  <p>EdTech</p>
+                </a>
+                <a href=""><img src ={icon3} className="icon"/>
+                  <p>Sustainable Development (Open Innovation)</p>
+                </a>
+                <a href=""><img src ={icon4} className="icon"/>
+                  <p>FinTech</p>
+                </a>
+            </div>
+    </div>)
 }
